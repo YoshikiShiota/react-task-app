6ec3a2c53c4dd6a5eb8d13be2ee55761ef702f83
YoshikiShiota <y.s3a6l9t@gmail.com>
2020-10-13 01:33:55 +0900

    [update]atoms, molecules分類のコンポーネント作成

---
+++ src/components/atoms/Submit.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Submit() {
+  return (
+    <button type="submit">Submit</button>
+  );
+}
+
+export default Submit;
---
+++ src/components/molecules/Search.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Input from '../atoms/Input';
+import Submit from '../atoms/Submit';
+
+function Search() {
+  return (
+    <React.Fragment>
+    <p>Searchコンポーネント</p>
+    <Input />
+    <Submit />
+    </React.Fragment>
+  );
+}
+
+export default Search;
